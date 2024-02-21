72c2ea9702a6b8095c84d1bbbf0e4526797d696f
Yoonjae-Moon <156685601+Yoonjae-Moon@users.noreply.github.com>
2024-02-22 01:38:53 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,40 @@
 const API_KEY = `503efe60dfbb4954bb3963baadbf8b56`;
-let news = []
-const getLatestNews = async()=>{
+let newsList = []
+const getLatestNews = async () => {
   const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
-                     );
-  
+  );
+
   const response = await fetch(url);
-  const data = await response.json()  
-  news = data.articles;
-  console.log("ddd", news);
+  const data = await response.json()
+  newsList = data.articles;
+  render();
+  console.log("ddd", newsList);
 };
 
+const render = () => {
+  const newsHTML = newsList.map(
+    news => `<div class="row news">
+  <div class="col-lg-4">
+    <img class="news-img-size" src=${news.urlToImage}>
+  </div>
+
+  <div class="col-lg-8">
+    <h2>
+      ${news.title}
+    </h2>
+    
+    <p>
+      ${news.description}
+    </p>
+    
+    <div>
+      ${news.source.name} * ${news.publishedAt}
+    </div>
+  </div>
+  
+</div>`).join('');
+
+
+  document.getElementById('news-board').innerHTML = newsHTML
+}
 getLatestNews();
