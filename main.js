c9438f60093ebea1f135e22a29108863980ac492
Yoonjae-Moon <156685601+Yoonjae-Moon@users.noreply.github.com>
2024-02-23 01:55:59 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 const API_KEY = `503efe60dfbb4954bb3963baadbf8b56`;
 let newsList = []
+const menus = document.querySelectorAll(".menus button")
+console.log("mmm", menus);
+menus.forEach(menu=>menu.addEventListener("click",(event)=>getNewsByCategory(event)))
+
 const getLatestNews = async () => {
-  const url = new URL(`https://yjtimes.netlify.app/`
+  const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
   );
 
   const response = await fetch(url);
@@ -10,6 +14,29 @@ const getLatestNews = async () => {
   render();
   console.log("ddd", newsList);
 };
+
+const getNewsByCategory = async(event)=>{
+  const category = event.target.textContent.toLowerCase();
+  console.log("category");
+  const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${API_KEY}`)
+  const response = await fetch(url)
+  const data = await response.json()
+  console.log("ddd", data)
+
+  newsList = data.articles;
+  render();
+}
+
+const getNewsByKeyword=async()=>{
+  const keyword = document.getElementById("search-input").value;
+  
+  console.log("keyword");
+  const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&q=${keyword}&apiKey=${API_KEY}`)
+
+  const response = await fetch (url)
+  const data = await response.json()
+  console.log("keyword data", data);
+}
 
 const render = () => {
   const newsHTML = newsList.map(
@@ -38,3 +65,7 @@ const render = () => {
   document.getElementById('news-board').innerHTML = newsHTML
 }
 getLatestNews();
+
+// 1. 버튼에 클릭 이벤트 주기
+// 2. 카테고리별 뉴스 가져오기
+// 3. 뉴스 보여주기
