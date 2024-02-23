1b6947707c10fd6a6c4c1c5f9c8431c2ef0cdae3
Yoonjae-Moon <156685601+Yoonjae-Moon@users.noreply.github.com>
2024-02-24 01:52:44 +0900

    Update main.js

--- main.js
+++ main.js
@@ -4,68 +4,103 @@ const menus = document.querySelectorAll(".menus button")
 console.log("mmm", menus);
 menus.forEach(menu=>menu.addEventListener("click",(event)=>getNewsByCategory(event)))
 
-const getLatestNews = async () => {
-  const url = new URL(`https://yjtimes.netlify.app/top-headlines?country=us&apiKey=${API_KEY}`
-  );
+let url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
 
-  const response = await fetch(url);
-  const data = await response.json()
-  newsList = data.articles;
-  render();
-  console.log("ddd", newsList);
-};
+//에러
+const getNews = async()=>{
+  try{
+    const response = await fetch(url);
 
-const getNewsByCategory = async(event)=>{
-  const category = event.target.textContent.toLowerCase();
-  console.log("category");
-  const url = new URL(`https://yjtimes.netlify.app/top-headlines?country=us&category=${category}&apiKey=${API_KEY}`)
-  const response = await fetch(url)
-  const data = await response.json()
-  console.log("ddd", data)
-
-  newsList = data.articles;
-  render();
+    const data = await response.json()
+    if (response.status === 200){
+      if (data.articles.length === 0){
+        throw new Error("No result for this search")
+      }
+      newsList = data.articles;
+      render();
+    } else{
+      throw new Error(data.message)
+    }    
+  }catch(error){
+    errorRender(error.message)
+  }
 }
 
+//뉴스 가져오기
+const getLatestNews = async () => {
+  url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
+
+  getNews();
+
+};
+
+//카테고리 분류
+const getNewsByCategory = async(event)=>{
+  const category = event.target.textContent.toLowerCase();
+  url = new URL(`https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${API_KEY}`)
+  getNews();
+}
+
+//키워드 검색
 const getNewsByKeyword=async()=>{
   const keyword = document.getElementById("search-input").value;
   
-  console.log("keyword");
-  const url = new URL(`https://yjtimes.netlify.app/top-headlines?country=us&q=${keyword}&apiKey=${API_KEY}`)
+  url = new URL(`https://newsapi.org/v2/top-headlines?country=us&q=${keyword}&apiKey=${API_KEY}`)
 
-  const response = await fetch (url)
-  const data = await response.json()
-  console.log("keyword data", data);
+  getNews();
 }
 
+//render 함수
 const render = () => {
-  const newsHTML = newsList.map(
-    news => `<div class="row news">
-  <div class="col-lg-4">
-    <img class="news-img-size" src=${news.urlToImage}>
-  </div>
-
-  <div class="col-lg-8">
-    <h2>
-      ${news.title}
-    </h2>
-    
-    <p>
-      ${news.description}
-    </p>
-    
-    <div>
-      ${news.source.name} * ${news.publishedAt}
-    </div>
-  </div>
+  const newsHTML = newsList.map(news =>
+    `<div class="row news">
+      <div class="col-lg-4">
+        <img class="news-img-size" src="${news.urlToImage || 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqEWgS0uxxEYJ0PsOb2OgwyWvC0Gjp8NUdPw&usqp=CAU'}">
+      </div>
+      <div class="col-lg-8">
+        <h2>${news.title}</h2>
+        <p>${
+          news.description && news.description.length > 200
+            ? news.description.substring(0, 200) + "..."
+            : news.description || "내용없음"
+        }</p>
+        <div>${news.source?.name || 'no source'} * ${news.publishedAt}</div>
+      </div>
+    </div>`
+  ).join('');
   
-</div>`).join('');
-
-
-  document.getElementById('news-board').innerHTML = newsHTML
+  document.getElementById('news-board').innerHTML = newsHTML;
 }
+
+const errorRender = (errorMessage) => {
+  const errorHTML = `<div class="alert alert-danger" role="alert">
+    ${errorMessage}
+  </div>`;
+
+  document.getElementById("news-board").innerHTML=errorHTML
+}
+
+const openNav = () => {
+  document.getElementById("mySidenav").style.width = "250px";
+};
+
+const closeNav = () => {
+  document.getElementById("mySidenav").style.width = "0";
+};
+
+const openSearchBox = () => {
+  let inputArea = document.getElementById("input-area");
+  if (inputArea.style.display === "inline") {
+    inputArea.style.display = "none";
+  } else {
+    
+    inputArea.style.display = "inline";
+  }
+};
+
 getLatestNews();
 
-// 1. 버튼에 클릭 이벤트 주기
-// 2. 카테고리별 뉴스 가져오기
-// 3. 뉴스 보여주기
+//햄버거 메뉴
+//검색 기능 생겼다 사라지기
+//검색결과 없을 시-done
+//이미지가 없을 시
